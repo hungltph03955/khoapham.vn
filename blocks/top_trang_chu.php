c02bf6b143855903adf9678f20ac354043e99a17
hungltph03955 <hungltph03955@fpt.edu.vn>
2017-02-20 01:44:19 +0700

    the working 29/2/2017

--- blocks/top_trang_chu.php
+++ blocks/top_trang_chu.php
@@ -7,7 +7,7 @@
                 $row_tinmoinha_mottin = mysql_fetch_array($tinmoinha_mottin);
               ?>
                 <img src="upload/tintuc/<?php echo $row_tinmoinha_mottin['urlHinh']; ?>"  /><br />
-                <h2 class="title"><a href="#"><?php echo $row_tinmoinha_mottin['TieuDe']; ?></a> </h2>
+                <h2 class="title"><a href="index.php?p=chitiettin&idTin=<?php echo $row_tinmoinha_mottin['idTin']; ?>"><?php echo $row_tinmoinha_mottin['TieuDe']; ?></a> </h2>
                 <div class="des">
                     <?php echo $row_tinmoinha_mottin['TomTat']; ?>
                 </div>
@@ -19,15 +19,19 @@
               <div class="content_scoller width_common">
             <ul>
 
-
+              <?php 
+                  $bontinmoi = TinMoiNhat_BonTin();
+                  while ( $rows_bontinmoi = mysql_fetch_array($bontinmoi)) {
+              ?>
                <li>
                 <div class="title_news">
-               		<a href="#" class="txt_link"> Bị bắt vì chụp ảnh selfie với váy ăn trộm </a> 
+               		<a href="index.php?p=chitiettin&idTin=<?php echo $rows_bontinmoi['idTin'] ?>" class="txt_link"><?php echo $rows_bontinmoi['TieuDe'] ?></a> 
                 </div>
               </li>
+              <?php 
+                }
+              ?>
               
-
-
             </ul>
             </div>			
             
--- lib/trangchu.php
+++ lib/trangchu.php
@@ -8,4 +8,24 @@
 		";
 		return mysql_query($qr);
 	}
+
+	function TinMoiNhat_BonTin() 
+	{
+	 $qr = "
+	 		SELECT idTin, TieuDe FROM tin 
+	 		ORDER BY idTin DESC 
+	 		LIMIT 1,10
+	 	";
+		return mysql_query($qr);
+	}
+	function TinXemNhieuNhat() 
+	{
+		$qr = "
+			SELECT * FROM tin 
+			ORDER BY SoLanXem DESC
+			LIMIT 0,6
+		";
+		return mysql_query($qr);
+	}
+
 ?>
